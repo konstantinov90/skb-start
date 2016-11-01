914b7700538e5a0a8aa23be92a5da4be9444270f
konstantinov90 <konstantinov90@list.ru>
2016-11-01 21:25:57 +0300

    v 0.1
    v 0.1

---
+++ index.js
@@ -0,0 +1,4 @@
+var Skb = require('skb');
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODE4ZDJiNTJmYjc0ZDAwMTFiZTdjYjUiLCJ1c2VybmFtZSI6ImtvbnN0YW50aW5vdjkwQGxpc3QucnUiLCJyb2xlIjoidXNlciIsImlhdCI6MTQ3ODAyMjU0M30.2E2NWZT9LXtC_HbuUMVHMgLv_ADbyttnCpZXm79VlpU';
+var skb = new Skb(token);
+skb.taskHelloWorld('konstantinov_skb_0');
